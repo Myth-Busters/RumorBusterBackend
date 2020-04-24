0239b8d48eee4d74a0ada0862a20197f59172aa9
Fawaz Alhenaki <faa5019@g.rit.edu>
2020-04-24 08:52:07 -0400

    fix rumor link bug

--- src/decider.py
+++ src/decider.py
@@ -140,7 +140,6 @@ def handle_request(request, flag):
       rumor.report_counter = rumor.report_counter  + 1
       rumor.save()
       return 3
-      return 400 #bad request, this should never happen.
   elif flag == '3':#garbage/other
     return 99 # Request will not be processed
 # TEST CALL
